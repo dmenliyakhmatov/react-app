254cdeee34deb659b050591d75febc2b38da9ffc
Dmitriy Menliyakhmatov <d.menliyakhmatov@space307.com>
2024-02-10 10:00:26 +0300

    update

--- example/index.js
+++ example/index.js
@@ -52,102 +52,125 @@ function createMacrotask(name) {
   });
 }
 
-// const fn1 = () => {
-//   const task = getTask('fn1');
-//   stackElement.prepend(task);
-//
-//   debugger;
-//
-//   task.remove();
-// };
-//
-// const fn2 = () => {
-//   const task = getTask('fn2');
-//   stackElement.prepend(task);
-//
-//   debugger;
-//
-//   fn1();
-//
-//   debugger;
-//
-//   task.remove();
-//   // createMicroTask('Micro 1');
-// };
-//
-// const fn3 = () => {
-//   const task = getTask('fn3');
-//   stackElement.prepend(task);
-//
-//   debugger;
-//
-//   fn2();
-//
-//   debugger;
-//
-//   task.remove();
-//   // createMacrotask('Macro 1');
-// };
+const fn1 = () => {
+  const task = getTask('fn1');
+  stackElement.prepend(task);
+
+  debugger;
+
+  task.remove();
+};
+
+const fn2 = () => {
+  const task = getTask('fn2');
+  stackElement.prepend(task);
+
+  debugger;
+
+  fn1();
+
+  debugger;
+
+  task.remove();
+};
+
+const fn3 = () => {
+  const task = getTask('fn3');
+  stackElement.prepend(task);
+
+  debugger;
+
+  fn2();
+
+  debugger;
+
+  task.remove();
+};
+// createMacrotask('Macro 1');
+
+// createMicroTask('Micro 1');
 
 // fn3();
 
 // createTask('Task 1');
-//
-// fetch(`https://68f241df693169c2.mokky.dev/articles/`)
-//   .then(res => {
-//     createTask('Fetch 1');
-//     return res.json();
-//   })
-//   .then(() => {
-//     createTask('Fetch 2');
-//   });
-// createMacrotask('Macro 1');
-//
-// createMicroTask('Micro 1');
-//
+
 // createMacrotask('Macro 2');
-//
+
 // createMicroTask('Micro 2');
 
 // console.log('Start of script');
-//
+
 // setTimeout(function () {
 //   console.log('Execute macrotask (setTimeout)');
 // }, 0);
-//
+
 // Promise.resolve().then(function () {
 //   console.log('Execute microtask (Promise)');
 // });
-//
+
 // console.log('End of script');
+
+//=========================================================================================================
 
 // const fn1 = () => {
 //   console.log('fn1 start');
 //   console.log('fn1 finish');
 // };
-//
+
 // const fn2 = () => {
 //   console.log('fn2 start');
 //   fn1();
 //   console.log('fn2 finish');
 // };
-//
+
 // const fn3 = () => {
 //   console.log('fn3 start');
 //   fn2();
 //   console.log('fn3 finish');
 // };
-//
+
+// Promise.resolve()
+//   .then(function () {
+//     console.log('Execute microtask (Promise)');
+//   })
+//   .then(() => {
+//     console.log('Execute microtask (Promise) 2');
+//   });
+
 // setTimeout(function () {
 //   console.log('Execute macrotask (setTimeout)');
 // }, 0);
-//
+
 // fn3();
 
-console.log('1');
+//=========================================================================================================
 
-const fn1 = () => {
-  console.log('fn1');
-};
+// console.log('1');
 
-fn1();
+// const fn2 = () => {
+//   debugger;
+//   console.log('fn2');
+//   debugger;
+// };
+
+// const fn1 = () => {
+//   debugger;
+//   fn2();
+//   debugger;
+//   console.log('fn1');
+//   debugger;
+// };
+
+// fn1();
+
+// setTimeout(() => {
+//   console.log('timeot');
+// }, 0);
+// fn1()
+///stack
+
+// const fn = () => {
+//   fn();
+// };
+
+// fn();
